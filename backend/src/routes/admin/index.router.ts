e23d3d0ff4a9d3b016991700ee3cdc990ccaf107
Nadeem-pc <nadeempc777@gmail.com>
2026-03-06 20:37:16 +0530

    feat(admin): add dashboard router to admin routes

--- backend/src/routes/admin/index.router.ts
+++ backend/src/routes/admin/index.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import jobsRouter from "./jobs.router";
+import dashboardRouter from "./dashboard.router";
 import userManagmentRouter from "./user-management.router";
 
 const adminRouter = Router();
 
 adminRouter.use('/', jobsRouter);
+adminRouter.use('/', dashboardRouter);
 adminRouter.use('/', userManagmentRouter);
 
 export default adminRouter;
